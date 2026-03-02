4c1483e67ae716866ffc7c8f9a044cf8820e2903
M1ckeyM0us <miketeamgamer13@gmail.com>
2026-03-02 08:34:05 -0600

    Started Sprint 2 + giving what I have to josh for favoriting

---
+++ Currency App/CountryNews.swift
@@ -0,0 +1,18 @@
+//
+//  CountryNews.swift
+//  Currency App
+//
+//  Created by MYKHAILO NAUMOV on 3/2/26.
+//
+
+import SwiftUI
+
+struct CountryNews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CountryNews()
+}
---
+++ Currency App/NewsService.swift
@@ -0,0 +1,8 @@
+//
+//  NewsService.swift
+//  Currency App
+//
+//  Created by MYKHAILO NAUMOV on 3/2/26.
+//
+
+import Foundation
